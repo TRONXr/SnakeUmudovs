5f836e3dc6f7a35d8915263a3f9467a6ed9acfd4
Вячеслав Торган <argmozgiblabr@mail.ru>
2024-10-17 15:54:36 +0500

    first commit

---
+++ SnakeUmudov/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeUmudov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
